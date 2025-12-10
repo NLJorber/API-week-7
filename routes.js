785a4f8762425e5ad6e12ba65b6612e788338934
SamR2406 <bedfordsr2@hotmail.com>
2025-12-10 14:34:43 +0000

    typos fixed

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { getAllMeds, createMed, getMedById, updateMedById, deleteMedById } = require("./medsController");
-const { signup, login } = require("./authController");
-const { createProfile, getProfiles, getProfileById, updateProfileById, deleteProfile } = require("./profileController");
+const { signUp, login } = require("./authController");
+const { createProfile, getProfiles, getProfileById, updateProfile, deleteProfile } = require("./profileController");
 const auth = require("./authMiddleware");
 
 //base url lh 3001
@@ -22,14 +22,14 @@ router.put("/:id", auth, updateMedById)
 router.delete("/:id", auth, deleteMedById);
 
 //This to authenticate user
-router.post("/auth/signup", signup);
+router.post("/auth/signup", signUp);
 router.post("/auth/login", login);
 
 //This to get user profile
 router.post("/profiles", auth, createProfile);
-router.get("/prodifles", auth, getProfiles);
+router.get("/profiles", auth, getProfiles);
 router.get("/profiles/:id", auth, getProfileById);
-router.put("/profiles/:id", auth, updateProfileById);
+router.put("/profiles/:id", auth, updateProfile);
 router.delete("/profiles/:id", auth, deleteProfile);
 
 
